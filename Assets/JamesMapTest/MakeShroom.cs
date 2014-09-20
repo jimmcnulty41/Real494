1e9dee068e917b18a6ec66ff8de5a99a1dfc5f2e
James McNulty <jimmcnulty41@gmail.com>
2014-09-19 23:58:26 -0400

    ShroomCube Stuff

---
+++ Assets/JamesMapTest/MakeShroom.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeShroom : MonoBehaviour {
+
+	public GameObject cubePrefab;
+	public GameObject shroomContainer;
+	public float xChange = .3f;
+	public float minX = .5f;
+	public float yChange = .15f;
+	public bool ________________________;
+	public float curYChange = 0;
+	public float curXChange = 0;
+
+	void Start(){
+		GameObject iterativeCube = null;
+		do {
+			GameObject downShroom = null;
+			if (iterativeCube){
+				downShroom = iterativeCube;
+			}
+			iterativeCube = Instantiate(cubePrefab) as GameObject;
+			iterativeCube.transform.position = shroomContainer.transform.position;
+			iterativeCube.transform.parent = shroomContainer.transform;
+			Vector3 pos = iterativeCube.transform.position;
+			pos.y += getY();
+			iterativeCube.transform.position = pos;
+			Vector3 scale = iterativeCube.transform.localScale;
+			scale.x -= getX();
+			iterativeCube.transform.localScale = scale;
+			ShroomCube sc = iterativeCube.GetComponent<ShroomCube>();
+			//	Set info common to all shroomcubes
+			sc.xDiff = xChange;
+			sc.yDiff = yChange;
+			sc.minX = minX;
+			sc.baseloc = gameObject.transform.position;
+			sc.baseScale = cubePrefab.transform.lossyScale;
+			sc.setNum();
+
+			//	Set up linked list of shrooms
+			sc.downShroom = downShroom;
+			if (sc.downShroom != null){
+				sc.downShroom.GetComponent<ShroomCube>().upShroom = iterativeCube;
+			}
+			if (shroomContainer.transform.childCount > 1000) break;
+		} while(iterativeCube.transform.localScale.x > minX);
+	}
+
+	float getY(){
+		curYChange += yChange;
+		return curYChange;
+	}
+
+	float getX(){
+		curXChange += xChange;
+		return curXChange;
+	}
+
+}
---
+++ Assets/JamesMapTest/ShroomCube.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShroomCube : MonoBehaviour {
+
+
+	public bool _______________________;
+	public GameObject upShroom = null;
+	public GameObject downShroom = null;
+	public float xDiff;
+	public float yDiff;
+	public float minX;
+	public Vector3 baseloc;
+	public Vector3 baseScale;
+	public Vector3 bottomLeftCorner;
+	public Vector3 topLeftCorner;
+	public Vector3 topRightCorner;
+	public Vector3 bottomRightCorner;
+	public Vector3 vLeftToCenter;
+	public Vector3 vRightToCenter;
+	public int number;
+	static public int numberCounter = 0;
+
+	public void setNum(){
+		number = numberCounter++;
+	}
+
+	void Start(){
+		bottomLeftCorner = new Vector3((baseloc.x - (baseScale.x / 2)), (baseloc.y + (baseScale.y / 2)),0);
+		bottomRightCorner = new Vector3((baseloc.x + (baseScale.x / 2)), (baseloc.y + (baseScale.y / 2)),0);
+		float cornerXDiff = .5f * (baseScale.x - minX);
+		float numBlocksish = cornerXDiff / xDiff;
+		topLeftCorner = new Vector3((baseloc.x - (minX / 2)), baseloc.y + (numBlocksish * yDiff), 0);
+		topRightCorner = new Vector3((baseloc.x + (minX / 2)), baseloc.y + (numBlocksish * yDiff), 0);
+		vLeftToCenter.x = 0;
+		vLeftToCenter = (topLeftCorner - bottomLeftCorner).normalized;
+		vLeftToCenter.y = 0;
+		vRightToCenter = (topRightCorner - bottomRightCorner).normalized;
+
+	}
+
+	public void modifyVel(GameObject mover, float speed){
+		PhysicsObject po = mover.GetComponent<PhysicsObject>();
+		if (mover.collider.bounds.max.x >= bottomLeftCorner.x 
+		    && mover.collider.bounds.max.x <= topLeftCorner.x
+		    && speed > 0){
+				po.vel += vLeftToCenter;
+		}
+		if (mover.collider.bounds.min.x >= bottomRightCorner.x 
+		    && mover.collider.bounds.min.x <= topRightCorner.x
+		    && speed < 0){
+				po.vel += vRightToCenter;
+		}
+	}
+
+}
